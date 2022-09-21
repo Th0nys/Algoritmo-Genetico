b03036590952dbfc00f3836a01dd2e1867136a38
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2022-09-21 10:06:37 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -155,34 +155,3 @@ def ordenar(populacao = [], adapt = []):
                 temp = adapt[i]
                 adapt[i] = adapt[j]
                 adapt[j] = temp
-
-
-gerarPop()
-mostraPop()
-mostraPopFil()
-i = 0
-while apt[3] != 4 and i < 20:
-    avaliarPop()
-    avaliarPopFil()
-    mostraApt()
-    ordenar(pop, apt)
-    mutacao()
-    avaliarPop()
-    mostraPop()
-    ordenar(pop, apt)
-    mostraPop()
-    avaliarPopFil()
-    cruzamento(selecao_roleta())
-    avaliarPopFil()
-    ordenar(popFil, aptFil)
-    cruzamento(selecao_roleta())
-    avaliarPopFil()
-    ordenar(popFil, aptFil)
-    mostraPop()
-    pop, apt = substituicao()
-    mostraPop()
-    mostraApt()
-    ordenar(pop, apt)
-    i = i + 1
-
-print(f'{i} gerações para achar soluções')
